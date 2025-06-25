2be124f16838dcb07c88496050fa9a737f9ebbb2
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-06-25 15:37:33 +0200

    add printmonth, update main method, add months, add dayspermonth

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,16 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
-    public static String[] daysOfWeek = {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"};
-    public static String[] months = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+    private static final String[] daysOfWeek = {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"};
+    private static final String[] months = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+    private static Integer[] daysPerMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     public static void main(String[] args) {
-        //System.out.println("07-Git - Aufgabe 2: Bonus: Eine Aufgabe wiederholen");
-        //System.out.println(add(1,1));
-
         System.out.println("07-Git - Bonus: Deluxe-Kalender");
-        calendar();
+        printCalendar();
     }
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static void calendar() {
+    public static void printCalendar(){
         Scanner userEntries = new Scanner(System.in);
         System.out.print("Bitte geben Sie das gewünschte Jahr als vierstellige Zahl ein:");
         String entry = userEntries.nextLine();
@@ -28,13 +23,23 @@ public class Main {
         calendar.set(year, Calendar.JANUARY,1);
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
-
-        printWeekDays();
+        int dayCounter = dayOfWeek;
+        for (int i = 0; i < 12; i++){
+            printMonth(i);
+        }
     }
 
     public static void printWeekDays() {
         for (int i = 0; i < 7; i++) {
             System.out.print(daysOfWeek[i] + " ");
         }
+        System.out.println();
+    }
+
+    public static void printMonth(int month) {
+        System.out.println(months[month]);
+        printWeekDays();
+        System.out.println();
+
     }
 }
