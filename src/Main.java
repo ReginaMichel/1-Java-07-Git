0a41ee9c91ddf2aa7f4686c87a4bbc83e99aa1be
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-06-25 11:48:12 +0200

    update Main.java added adding method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("07-Git - Aufgabe 2: Bonus: Eine Aufgabe wiederholen");
+        System.out.println(add(1,1));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
